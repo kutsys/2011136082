5d8c645d4564ce3be62ec7765624fdf40b6b400f
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-10-31 18:43:43 +0900

    Create bar5.c

---
+++ bar5.c
@@ -0,0 +1,8 @@
+#include "libfooboobar.h"
+
+int InBar(int num1, int num2) {
+    printf("You are in Bar!!!\n");
+    printf("Foo returns the results of multiplication.\n");
+
+    return num1 * num2;
+}
